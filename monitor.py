79b52da3357260ae4dc26b62ea1aebf959d54632
Matthew Northcott <matthewnorthcott@catalyst.net.nz>
2020-01-11 14:26:04 +1300

    Removed buzzer functionality and f-strings for Python-3.5 compatibility

--- monitor.py
+++ monitor.py
@@ -18,7 +18,6 @@ import weather
 import flickuser
 
 # GLOBALS
-GPIO_BUZZER = 22
 GPIO_LED_GREEN = 9
 GPIO_LED_ORANGE = 10
 GPIO_LED_RED = 11
@@ -32,7 +31,6 @@ conf = SourceFileLoader("conf", FILE_CONFIG).load_module()
 
 
 class Monitor(object):
-    enable_buzzer = True
     led_state = False
     running = False
 
@@ -40,14 +38,12 @@ class Monitor(object):
         # GPIO initialisation
         GPIO.setmode(rpi.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(GPIO_BUZZER, GPIO.OUT)
         GPIO.setup(GPIO_LED_GREEN, GPIO.OUT)
         GPIO.setup(GPIO_LED_ORANGE, GPIO.OUT)
         GPIO.setup(GPIO_LED_RED, GPIO.OUT)
         GPIO.setup(GPIO_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         self.startup()
-        GPIO.add_event_detect(GPIO_BUTTON, GPIO.RISING, callback=self.toggle_buzzer, bouncetime=200)
 
         self.lcd = lcd.LCD()
         self.weather = weather.WeatherData()
@@ -87,15 +83,8 @@ class Monitor(object):
             GPIO.output(GPIO_LED_ORANGE, GPIO.HIGH)
             GPIO.output(GPIO_LED_RED, GPIO.HIGH)
             time.sleep(0.5)
-
-    def toggle_buzzer(self):
-        self.enable_buzzer = not self.enable_buzzer
-
-        if not self.enable_buzzer:
-            GPIO.output(GPIO_BUZZER, GPIO.HIGH)
     
     def reset_leds(self):
-        GPIO.output(GPIO_BUZZER, GPIO.HIGH)
         GPIO.output(GPIO_LED_GREEN, GPIO.HIGH)
         GPIO.output(GPIO_LED_ORANGE, GPIO.HIGH)
         GPIO.output(GPIO_LED_RED, GPIO.HIGH)
@@ -104,18 +93,10 @@ class Monitor(object):
         self.reset_leds()
 
         if self.flick.spot_price >= conf.PRICE_LIMIT_UPPER:
-            state = GPIO.HIGH if self.led_state else GPIO.LOW
-
-            GPIO.output(GPIO_LED_RED, state)
-
-            if self.enable_buzzer:
-                GPIO.output(GPIO_BUZZER, state)
-
+            GPIO.output(GPIO_LED_RED, GPIO.HIGH if self.led_state else GPIO.LOW)
             self.led_state = not self.led_state
-
         elif self.flick.spot_price < conf.PRICE_LIMIT_LOWER:
             GPIO.output(GPIO_LED_GREEN, GPIO.HIGH)
-            
         else:
             GPIO.output(GPIO_LED_ORANGE, GPIO.HIGH)
 
@@ -123,21 +104,21 @@ class Monitor(object):
         self.weather.update()
         self.flick.update()
 
-        temperature = f"{self.weather.temperature}C"
-        humidity = f"{self.weather.humidity}%"
-        rainfall = f"{self.weather.rainfall}mm"
-        wind_gust = f"{self.weather.wind_speed_gust:.0f}km/h"
-        wind_mean = f"{self.weather.wind_speed_mean:0f}km/h"
+        temperature = "{}C".format(self.weather.temperature)
+        humidity = "{}%".format(self.weather.humidity)
+        rainfall = "{}mm".format(self.weather.rainfall)
+        wind_gust = "{:.0f}km/h".format(self.weather.wind_speed_gust)
+        wind_mean = "{:0f}km/h".format(self.weather.wind_speed_mean)
         wind_dir = self.weather.wind_dir
 
-        price = f"{self.flick.price:.2f}c"
-        spot_price = f"{self.flick.spot_price:.2f}c"
+        price = "{:.2f}c".format(self.flick.price)
+        spot_price = "{:.2f}c".format(self.flick.spot_price)
 
         lines = [
             datetime.datetime.now().strftime(TIME_FORMAT),
-            f"{price:<10}{spot_price:>10}",
-            f"{temperature:<9}{humidity:<5}{rainfall:>6}",
-            f"{wind_gust:<9}{wind_mean:<7}{wind_dir:>4}",
+            "{:<10}{:>10}".format(price, spot_price),
+            "{:<9}{:<5}{:>6}".format(temperature, humidity, rainfall),
+            "{:<9}{:<7}{:>4}".format(wind_gust, wind_mean, wind_dir),
         ]
 
         for i in range(len(lines)):
